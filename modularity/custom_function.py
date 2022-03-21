4bdcbbea33ad3e34bdb59fff8c13aca9c4089520
Iulian <iulianboerescu16@gmail.com>
2022-03-21 21:16:03 +0200

    My second commit

---
+++ modularity/custom_function.py
@@ -0,0 +1,56 @@
+# def my_function():
+#     print("message from my funct.")
+#
+#
+# my_function()
+# print("end of program")
+
+
+# def my_sum(nr1, nr2):
+#     print("sum of nr = ", nr1+nr2)
+#
+#
+# x=20
+# y=23
+# my_sum(x, y)
+
+# def my_sum(a, b):
+#     return a+b
+#
+#
+# my_sum(12, 7)
+# print(my_sum(12,7))
+
+
+# def s(a, b, c=100, d=200):
+#     print(a, b, c, d)
+#
+#
+# s(10, 7)
+# s(10, 7, -20)
+# s(10, 7, d=-20)
+# s(b=10, a=7, d=-20)
+
+# def my_function(*args, **kwargs):
+#     print("my func", args, kwargs)
+#
+#
+# my_function()
+# my_function(1,2)
+# my_function(1,2,c=3)
+# my_function([1,2,3], ('a', 'b', 'c'), another_key=set([True, False, None]))
+
+
+# def my_function(param1, param2, *args, keyword_1=None, keyword_2=None, **kwargs):
+#     print(param1, param2, args, keyword_1,keyword_2, kwargs)
+#
+#
+# my_function(1, 2, "a", keyword_1="abc", keyword_3="this goes to kwargs")
+
+
+# def f(a, b, c, *args, a1, b1, c1, **kwargs):
+#     print(a, b, c, args, a1,b1, c1, kwargs)
+#
+#
+# f(*[1,2,3,4,5,6], **{"a1":1, "b1":2, "c1":3, "d1":4})
+
